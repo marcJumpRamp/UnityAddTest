adabd8759cd564b2bb8521132ababf4aad9586e2
marc <marc@jumprampgames.com>
2016-08-23 16:29:09 -0400

    Added AppsLovin logic. Still need to set up on dashboard to see ads. Made minor changes to Facebook Ad script.

--- Assets/Scripts/AppLovin_JRG.cs
+++ Assets/Scripts/AppLovin_JRG.cs
@@ -6,13 +6,17 @@ public class AppLovin_JRG : MonoBehaviour {
 
     public void loadAppLovinAd()
     {
+        ALStatusNotifier = "Attempting to load Apps Lovin Video";
+        Debug.Log("Attempting to load Apps Lovin Video");
         AppLovin.PreloadInterstitial();
 
         if (AppLovin.HasPreloadedInterstitial())
         {
             ALStatusNotifier = "Apps Lovin Video is playing";
+            Debug.Log("Apps Lovin Video is playing");
             AppLovin.ShowInterstitial();
         }
+        else { ALStatusNotifier = "Apps Lovin Video is not available"; Debug.Log("Apps Lovin Video is not available"); }
     }
 
     void onAppLovinEventReceived(string eventId)
@@ -23,13 +27,16 @@ public class AppLovin_JRG : MonoBehaviour {
             case "HIDDENINTER":
             case "HIDDENREWARDED":
                 ALStatusNotifier = "Apps Lovin Video has been hidden";
+                Debug.Log("Apps Lovin Video has been hidden");
                 break;
             case "LOADINTERFAILED":
             case "LOADREWARDEDFAILED":
                 ALStatusNotifier = "Apps Lovin Video failed to load";
+                Debug.Log("Apps Lovin Video failed to load");
                 break;
             case "REWARDAPPROVED":
                 ALStatusNotifier = "Apps Lovin Rewarded Video has been approved";
+                Debug.Log("Apps Lovin Rewarded Video has been approved");
                 break;
         }
     }
